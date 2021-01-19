7c0ec582fd994b39f9b5bb699f13348395f10c4a
MiqdadMods <77543167+MiqdadMods@users.noreply.github.com>
2021-01-19 11:34:58 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,17 @@
+const donasi = (prefix) => {
+	return `╔════════════════════
+║ *DONASI UNTUK BOT*
+╠════════════════════
+║╭──❉ *DONASI BOS* ❉─────
+║│➸  Pulsa  :  6285326364468
+║│
+║│
+║│
+║╰──────────────────
+╠════════════════════
+║ _*Miqdad Mods Bot**_
+╚════════════════════
+`
+}
+
+exports.donasi = donasi
---
+++ src/help.js
@@ -0,0 +1,83 @@
+const help = (prefix) => {
+	return `🔰Miqdad Mods Menu🔰
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ *⚠️ NO SPAM! NO CALL!!! ⚠️*
+┃      *LANGGAR AUTO BLOCK*
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⊱❥ OWNER : Miqdad Mods
+┃⊱❥ YOUTUBE : Miqdad Mods
+┃⊱❥ PREFIX : 「 ${prefix} 」
+┃⊱❥ VERSION : 6.0
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Tentang Bot
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}info
+┃➢ ${prefix}owner
+┃➢ ${prefix}donasi
+┃➢ ${prefix}blocklist
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Fun Menu
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}sticker
+┃➢ ${prefix}sticker nobg
+┃➢ ${prefix}toimg
+┃➢ ${prefix}ttp
+┃➢ ${prefix}tts
+┃➢ ${prefix}nulis
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Download Menu
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}tiktod
+┃➢ ${prefix}ytmp3
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Meme Menu
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}meme
+┃➢ ${prefix}memeindo
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Group Menu
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}admin
+┃➢ ${prefix}welcome [1/0]
+┃➢ ${prefix}add
+┃➢ ${prefix}kick
+┃➢ ${prefix}promote
+┃➢ ${prefix}demote
+┃➢ ${prefix}tagall
+┃➢ ${prefix}tagall2
+┃➢ ${prefix}tagall3
+┃➢ ${prefix}linkgrup
+┃➢ ${prefix}leave
+┃➢ ${prefix}simih [1/0]
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Tobat Lord[ EMROR ]
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}loli
+┃➢ ${prefix}nsfwloli
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Other Menu
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}quotes
+┃➢ ${prefix}ssweb
+┃➢ ${prefix}simi
+┃➢ ${prefix}ocr
+┃➢ ${prefix}wait
+┃➢ ${prefix}tiktokstalk
+┃➢ ${prefix}hilih
+┃➢ ${prefix}ytstalk
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ Owner Menu:v
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}setprefix
+┃➢ ${prefix}bc
+┃➢ ${prefix}clearall
+┃➢ ${prefix}clone
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ *Miqdad Mods Bot*
+┣━━━━━━━━━━━━━━━━━━━━┓
+`
+}
+
+exports.help = help
